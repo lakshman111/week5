738f5484729779076995bc25eed573e0cf041b3e
Lakshman Mody <lakshman111@gmail.com>
2016-02-11 17:24:52 -0600

    fixed the Book thing, it works yay

--- book_database.py
+++ book_database.py
@@ -1,13 +1,7 @@
 import csv
 
 class Book:
-  # has to be a better way to do this since its repetative
   publishers = []
-  with open("books.csv", encoding='utf-8') as file:
-      reader = csv.reader(file)
-      for row in reader:
-        publishers.append(row[3])
-
 
   def search(term):
     books = []
@@ -21,6 +15,7 @@ class Book:
           book.title = row[1]
           book.author = row[2]
           book.publisher = row[3]
+          Book.publishers.append(book.publisher)
           books.append(book)
     for book in books:
       if term.lower() in book.title.lower():
